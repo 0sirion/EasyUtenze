c561f916a6c29a0b6afa9bbd6449e03a5ac5b252
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-25 11:34:29 +0200

    applicazione

---
+++ Bollette.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math as plt
+
+
+st.header("Calcolatore Bollette")
+st.subheader(
+    "Puoi utilizzare quest'applicazione per visualizzare le tue bollette")
+st.write('Le bollette saranno divise per tipo e verranno forniti dei grafici per visualizzarne meglio i dati in ordine cronologico')
+
+
+def main():
+    print("hello")
+
+
+if __name__ == __main__:
+    main()
