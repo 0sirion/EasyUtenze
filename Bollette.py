4a9780c82e00daa6fce65c2b2ac93bfaae082315
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-25 12:57:44 +0200

    s

--- Bollette.py
+++ Bollette.py
@@ -19,7 +19,7 @@ def acqua_list_filler():
     acqua_list.append(acqua_value)
     st.write(acqua_list)
 
-acqua_value = st.input(label="inserisci il valore delle utenze dell'acqua: ")
+acqua_value = st.number_input(label="inserisci il valore delle utenze dell'acqua: ")
 acqua_date = st.date_input(label="inserisci la data")
 st.button(label="Premi per salvare il costo dell'utenza e la data", on_click=acqua_list_filler())
 
