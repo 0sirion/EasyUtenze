9cc734b528e0e38cf23b107b36212ed6d81ddf54
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-25 12:22:05 +0200

    s

--- Bollette.py
+++ Bollette.py
@@ -9,7 +9,6 @@ st.subheader(
     "Puoi utilizzare quest'applicazione per visualizzare le tue bollette")
 st.write('Le bollette saranno divise per tipo e verranno forniti dei grafici per visualizzarne meglio i dati in ordine cronologico')
 
-st.button(label="Premi per salvare il costo dell'utenza e la data")
 
 acqua = [12, 22]
 data_acqua=[]
@@ -17,6 +16,8 @@ data_acqua=[]
 def acqua_list_filler():
     st.number_input(label="inserisci il valore delle utenze dell'acqua: ", )
     st.date_input(label="inserisci la data")
+    st.button(label="Premi per salvare il costo dell'utenza e la data")
+
 
 def main():
     acqua_list_filler()
