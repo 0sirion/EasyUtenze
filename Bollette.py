9a3c3f4f7535b9e128bbb0c593d31658eff6e371
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-25 11:39:15 +0200

    s

--- Bollette.py
+++ Bollette.py
@@ -4,7 +4,7 @@ import numpy as np
 import math as plt
 
 
-st.header(":red[Calcolatore Bollette]")
+st.header(":#CD5C5C[Calcolatore Bollette]")
 st.subheader(
     "Puoi utilizzare quest'applicazione per visualizzare le tue bollette")
 st.write('Le bollette saranno divise per tipo e verranno forniti dei grafici per visualizzarne meglio i dati in ordine cronologico')
@@ -14,5 +14,5 @@ def main():
     print("hello")
 
 
-if __name__ == __main__:
+if __name__ == '__main__':
     main()
